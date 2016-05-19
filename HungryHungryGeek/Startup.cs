99e7f8ec97ce0fe325a88555eda4b21fb15e339b
Piotr Ostrowski <piotr.marcin.ostrowski@gmail.com>
2016-05-19 15:29:21 +0200

    Add project files.

---
+++ HungryHungryGeek/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HungryHungryGeek.Startup))]
+namespace HungryHungryGeek
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
